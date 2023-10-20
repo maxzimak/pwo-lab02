f7605d360a097842471167e5cbc65fe65f74a724
Maksym Zimak <maksym.zimak@pollub.edu.pl>
2023-10-20 16:30:18 +0200

    Zadanie 1 – scalenie

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -10,44 +10,8 @@
          boolean ok = SequenceTools.writeToFile(
              new FibonacciGenerator(), FROM, TO, FILE_NAME);
 
-<<<<<<< HEAD
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-            for (int i = 0; i <= 100; i++)
-                writer.write(Fibonacci(i).toString() + "\n");
-            writer.close();
-
-        } catch (IOException ex) {
-            System.out.println("Błąd");
-            return;
-        }
-
-        System.out.println("Wynik zapisany do pliku: " +
-            fileName);
-    }
-    static BigDecimal Fibonacci(int n) {
-
-        if (n == 0) return new BigDecimal(0);
-        if (n == 1) return new BigDecimal(1);
-
-        BigDecimal f0 = new BigDecimal(0);
-        BigDecimal f1 = new BigDecimal(1);
-
-        for (int i = 2; i <= n; i++) {
-
-            BigDecimal m = new BigDecimal(0);
-            m = m.add(f1);
-            m = m.add(f0);
-            f0 = f1;
-            f1 = m;
-        }
-        return f1;
-    }
-}
-=======
          if (ok) System.out.println("Wynik zapisany do pliku: " +
              FILE_NAME);
          else System.out.println("Błąd");
      }
  }
->>>>>>> zad1v2
